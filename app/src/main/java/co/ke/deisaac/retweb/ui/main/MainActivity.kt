58702c649300f218d0c7378a0c6cc13318c0506c
De Isaac <deisaack@gmail.com>
2020-08-17 03:17:18 +0300

    set text

--- app/src/main/java/co/ke/deisaac/retweb/ui/main/MainActivity.kt
+++ app/src/main/java/co/ke/deisaac/retweb/ui/main/MainActivity.kt
@@ -1,146 +1,146 @@
 package co.ke.deisaac.retweb.ui.main
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
-import android.widget.Toast
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
-import androidx.lifecycle.lifecycleScope
 import co.ke.deisaac.retweb.R
-import co.ke.deisaac.retweb.data.model.Post
-import co.ke.deisaac.retweb.data.repository.MainRepository
-import co.ke.deisaac.retweb.data.utils.Status
-import co.ke.deisaac.retweb.ui.base.ViewModelFactory
-import kotlinx.coroutines.launch
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import kotlin.collections.MutableMap as MutableMap
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+import java.nio.charset.Charset
+import java.util.*
 
 
 internal class MainActivity : AppCompatActivity() {
-    private val mainRepository = MainRepository()
-    lateinit var resultTextView: TextView;
-    private lateinit var viewModel: MainViewModel
-
-    companion object {
-        private const val TAG = "MainActivity"
-    }
+    var bluetoothAdapter: BluetoothAdapter? = null
+    var bluetoothDevice: BluetoothDevice? = null
+    var bluetoothSocket: BluetoothSocket? = null
+    var inputStream: InputStream? = null
+    var outputStream: OutputStream? = null
+    var stopWorker: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        resultTextView = findViewById(R.id.tv_result)
-        setUpViewModel()
-        setupObservers()
-        getPost()
+        findBluetoothDevice()
     }
 
-    fun setUpViewModel() {
-        viewModel = ViewModelProviders.of(this, ViewModelFactory())
-            .get(MainViewModel::class.java)
-    }
+    private fun findBluetoothDevice() {
+        try {
+            bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+            if (bluetoothAdapter == null) {
+                lblPrinterName.setText("No Bluetooth Adapter found")
+            }
+            if (bluetoothAdapter!!.isEnabled()) {
+                val enableBT = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                startActivityForResult(enableBT, 0)
+            }
+            val pairedDevice: Set<BluetoothDevice> =
+                bluetoothAdapter!!.getBondedDevices()
+            if (pairedDevice.size > 0) {
+                for (pairedDev in pairedDevice) {
 
-    private fun getPost() {
-        lifecycleScope.launch {
-            val call = mainRepository.getPost(1)
-            call.enqueue(object : Callback<Post?> {
-                override fun onResponse(call: Call<Post?>, response: Response<Post?>) {
-                    if (!response.isSuccessful()) {
-                        resultTextView.append( "\n\nGetPost\nCode: " + response.code())
-                        return
-                    }
-                    val post: Post? = response.body()
-                    val content = "Post id: " + post?.id + "\nUserId: " +
-                            post?.userId + "\nTitle: " + post?.title + "\nText: " + post?.text
-                    resultTextView.append(content)
-                    createPost()
-                }
-
-                override fun onFailure(call: Call<Post?>, t: Throwable) {
-                    resultTextView.text = t.message
-                }
-            })
-        }
-    }
-
-    private fun getPosts() {
-        lifecycleScope.launch {
-            val params: MutableMap<String?, String?> = HashMap()
-            params["_sort"] = "userId"
-            val call = mainRepository.getPosts(params)
-            call?.enqueue(object : Callback<List<Post?>?> {
-                override fun onFailure(call: Call<List<Post?>?>, t: Throwable) {
-                    TODO("Not yet implemented")
-                }
-
-                override fun onResponse(call: Call<List<Post?>?>, response: Response<List<Post?>?>) {
-                    if (response.isSuccessful) {
-                        val data = response.body()
-                        resultTextView.append("List returned status : " + response.code().toString())
-                    }
-                }
-            })
-        }
-    }
-
-    private fun setupObservers() {
-        viewModel.getUsers().observe(this, Observer {
-            it?.let { resource ->
-                when (resource.status) {
-                    Status.SUCCESS -> {
-                        val users = resource.data
-                        resultTextView.append("Users " + users?.count().toString())
-                        Log.d(TAG, resource.toString())
-                    }
-                    Status.ERROR -> {
-                        Log.d(TAG, it.message.toString())
-                        Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
-                    }
-                    Status.LOADING -> {
-                        Log.d(TAG, "Loading")
+                    // My Bluetoth printer name is BTP_F09F1A
+                    if (pairedDev.name == "BTP_F09F1A") {
+                        val bluetoothDevice = pairedDev
+                        lblPrinterName.setText("Bluetooth Printer Attached: " + pairedDev.name)
+                        break
                     }
                 }
             }
-        })
+            lblPrinterName.setText("Bluetooth Printer Attached")
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+        }
     }
 
-    private fun createPost() {
-        lifecycleScope.launch {
-//            Option 1: Create a Post instance and submit it
-//            val post = Post(23, "20", "Hi Title", text = "Hello world")
-//            val call: Call<Post> = mainRepository.createPost(post)
+    @Throws(IOException::class)
+    fun openBluetoothPrinter() {
+        try {
 
-//            Option 2: create a field map of the available fields and submit
-//            val fields: MutableMap<String, String> = HashMap()
-//            fields["userId"] = "29"
-//            fields["title"] = "Title is this"
-//            fields["body"] = "Happy new year"
-//            val f = fields as Map<String?, String?>
-//            val call: Call<Post?> = mainRepository.createPost(f)
+            //Standard uuid from string //
+            val uuidSting: UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb")
+            bluetoothSocket = bluetoothDevice?.createRfcommSocketToServiceRecord(uuidSting)
+            bluetoothSocket?.connect()
+            outputStream = bluetoothSocket?.outputStream
+            inputStream = bluetoothSocket?.inputStream
+            beginListenData()
+        } catch (ex: java.lang.Exception) {
+        }
+    }
 
-//           Option 3: pass the required fields as parameters in the function
-            val call: Call<Post?> = mainRepository.createPost(
-                userId = 12, title = "Hey there title", text = "Fantastic text")
-            call.enqueue(object : Callback<Post?> {
-                override fun onResponse(call: Call<Post?>, response: Response<Post?>) {
-                    if (!response.isSuccessful()) {
-                        resultTextView.append("\n\nPost Code: " + response.code())
-                        return
+    private fun beginListenData() {
+        try {
+            val handler = Handler()
+            val delimiter: Byte = 10
+            stopWorker = false
+            var readBufferPosition = 0
+            val readBuffer = ByteArray(1024)
+            val thread = Thread(Runnable {
+                while (!Thread.currentThread().isInterrupted && !stopWorker) {
+                    try {
+                        val byteAvailable: Int = inputStream!!.available()
+                        if (byteAvailable > 0) {
+                            val packetByte = ByteArray(byteAvailable)
+                            inputStream?.read(packetByte)
+                            for (i in 0 until byteAvailable) {
+                                var b = packetByte[i]
+                                if (b == delimiter) {
+                                    val encodedByte =
+                                        ByteArray(readBufferPosition)
+                                    System.arraycopy(
+                                        readBuffer, 0,
+                                        encodedByte, 0,
+                                        encodedByte.size
+                                    )
+                                    val data = String(encodedByte, Charset.defaultCharset())
+                                    readBufferPosition = 0
+                                    handler.post(Runnable { lblPrinterName.setText(data) })
+                                } else {
+                                    b = readBuffer.get(readBufferPosition)
+//                                    readBuffer.get(readBufferPosition++) = b
+                                }
+                            }
+                        }
+                    } catch (ex: java.lang.Exception) {
+                        stopWorker = true
                     }
-                    val resp = response.body()
-                    var content = ""
-                    content +=  "\n\n\nhhhhh" + resp.toString()
-                    resultTextView.append(content)
-                    getPosts()
-                }
-
-                override fun onFailure(call: Call<Post?>?, t: Throwable) {
-                    resultTextView.append(t.message)
                 }
             })
+            thread.start()
+        } catch (ex: java.lang.Exception) {
+            ex.printStackTrace()
+        }
+    }
+
+    @Throws(IOException::class)
+    fun printData() {
+        try {
+            var msg: String = editTextContent.text.toString()
+            msg += "\n"
+            outputStream!!.write(msg.toByteArray())
+            lblPrinterName.text = "Printing Text..."
+        } catch (ex: java.lang.Exception) {
+            ex.printStackTrace()
+        }
+    }
+
+    // Disconnect Printer //
+    @Throws(IOException::class)
+    fun disconnectBT() {
+        try {
+            stopWorker = true
+            outputStream!!.close()
+            inputStream!!.close()
+            bluetoothSocket?.close()
+            lblPrinterName.text = "Printer Disconnected."
+        } catch (ex: java.lang.Exception) {
+            ex.printStackTrace()
         }
     }
 }
